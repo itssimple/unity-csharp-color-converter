e45c8777f126a824d5952dbca39d3f269ce6d254
NoLifeKing <github-things@nolifeking85.tv>
2025-07-04 14:01:31 +0200

    fix: Hopefully the production site will work better now :D

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => {
     return {
+        plugins: [tailwindcss()],
         resolve: {
             alias: {
                 "@": path.resolve(__dirname, "."),
